fa05cd0d90aa500563b239d7657fde47265da486
Qurban Ali <qurban.girhore@gmail.com>
2026-06-16 21:58:18 +0500

    Update 06_Executive_Dashboard.py

--- pages/06_Executive_Dashboard.py
+++ pages/06_Executive_Dashboard.py
@@ -338,7 +338,7 @@ def main():
                 y='gpa',
                 color='department',
                 size='fee_allocated',
-                color_discrete_sequence=px.colors.pastel.Pastel,
+                color_discrete_sequence=px.colors.pastel.Pastel,  # ❌ Yeh galat hai
                 labels={'attendance': 'Attendance (%)', 'gpa': 'Student Cumulative GPA'},
                 opacity=0.6
             )
